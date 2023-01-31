c6abb4ccacd428df990bb61dfa576c6c45df0688
StepanTret <75583948+StepanTret@users.noreply.github.com>
2023-01-31 16:18:32 +0300

    Add files via upload

---
+++ sites3/businesssite/js/script.js
@@ -0,0 +1,40 @@
+
+
+
+let animItems = document.querySelectorAll('._anim-items');
+
+if(animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(){
+        for(let index = 0; index < animItems.length; index++){
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight; 
+            const animItemsOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((scrollY > animItemsOffset - animItemPoint) && scrollY < (animItemsOffset + animItemHeight)){
+                animItem.classList.add('_active');
+            } else{
+                if(!animItem.classList.contains('_anim-no-hide')){
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el){
+        const rect = el.getBoundingClientRect();
+         var   scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+         var   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 900);
+    
+}
+
